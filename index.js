36efa5f72b5704d2b6071218a523ab36ec704e61
overjjang99 <overjjang99@gmail.com>
2024-12-26 21:31:59 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
