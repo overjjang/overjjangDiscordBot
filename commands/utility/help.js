8e2c4d7f642eeb0e496782f99aa16ddb91be6a43
overjjang99 <overjjang99@gmail.com>
2025-03-11 10:03:25 +0900

    chore : 상세한 도움말 구현중. 선택창 기능 구현, 상세 명령어 정보 json파일 제작

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -1,4 +1,6 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder,StringSelectMenuBuilder,StringSelectMenuOptionBuilder,ActionRowBuilder } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
 
 const data = new SlashCommandBuilder()
     .setName('도움말')
@@ -12,21 +14,37 @@ const data = new SlashCommandBuilder()
         'en-US': 'Check the command list'
     });
 
+const jsonPath = path.join(__dirname, '../../commandInfo.json');
+
+function readJsonFile() {
+    if (fs.existsSync(jsonPath)) {
+        const data = fs.readFileSync(jsonPath, 'utf8');
+        return JSON.parse(data);
+    } else {
+        return [];
+    }
+}
+
 module.exports = {
     category: 'utility',
     data : data,
     async execute(interaction) {
+        const commandList = readJsonFile();
+        console.log(commandList);
         const helpEmbed = new EmbedBuilder()
             .setColor('#0099ff')
             .setTitle('명령어 목록')
-            .setDescription('명령어 목록을 확인합니다')
-            .setFields(
-                // 자동으로 명령어 목록을 생성합니다
-                interaction.client.commands.map(command => ({
-                    name: "/" + command.data.name,
-                    value: command.data.description,
-                }))
+            .setDescription('아레에서 명령어를 선택해주세요')
+        const selectMenu = new StringSelectMenuBuilder()
+            .setCustomId('helpSelect')
+            .setPlaceholder('명령어를 선택해주세요')
+            .addOptions(
+                commandList.commands.map(item => new StringSelectMenuOptionBuilder()
+                    .setLabel(item.name)
+                    .setValue(item.name)
+                    .setDescription(item.description)
+                )
             )
-        interaction.reply({ embeds: [helpEmbed] });
+        const respons = interaction.reply({ embeds: [helpEmbed], components: [new ActionRowBuilder().addComponents(selectMenu)] });
     }
 }
