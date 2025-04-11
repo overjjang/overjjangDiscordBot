e4778e316d3e6c130d5b7d848b80798bdc286425
overjjang99 <overjjang99@gmail.com>
2025-04-11 11:57:57 +0900

    chore : added description of embedPrefix.js

--- modules/embedPrefix.js
+++ modules/embedPrefix.js
@@ -10,7 +10,16 @@ function FieldsBuilder(name, value, inline = false) {
 }
 
 const prefixEmbed = {
-    // 임베드 베이스 생성(title, description, color, ...fields)
+    /**
+     * 기본적인 임베드 메시지를 생성합니다.
+     * @async
+     * @function embedBase
+     * @param {string} title - 임베드의 제목
+     * @param {string} description - 임베드의 설명
+     * @param {string} [color=cm.success] - 임베드의 색상 (기본값: 성공 색상)
+     * @param {...Array<Object|FieldsBuilder>} fields - 임베드에 추가할 필드들
+     * @returns {Promise<EmbedBuilder>} 생성된 임베드 객체
+     */
     embedBase: async (title, description, color = cm.success, ...fields) => {
         const embed = new EmbedBuilder()
             .setColor(color)
@@ -30,18 +39,29 @@ const prefixEmbed = {
         }
         return embed;
     },
-    // 에러 임베드 생성(description)
+    /**
+     * 에러 메시지를 포함한 임베드를 생성합니다.
+     * @function errorEmbed
+     * @param {string} description - 에러 메시지
+     * @returns {EmbedBuilder} 에러 메시지 임베드 객체
+     */
     errorEmbed: (description) =>
         new EmbedBuilder()
             .setColor(cm.danger)
             .setTitle(":warning:ERROR")
             .setDescription(description),
-    // 경고 임베드 생성(description)
-    warningEmbed: (description) =>
-        new EmbedBuilder()
-            .setColor(cm.warning)
-            .setTitle(":warning:WARNING")
-            .setDescription(description),
+
+    /**
+     * 경고 메시지를 포함한 임베드를 생성합니다.
+     * @function warningEmbed
+     * @param {string} description - 경고 메시지
+     * @returns {EmbedBuilder} 경고 메시지 임베드 객체
+    */
+     warningEmbed: (description) =>
+            new EmbedBuilder()
+                .setColor(cm.warning)
+                .setTitle(":warning:WARNING")
+                .setDescription(description),
 };
 
 module.exports = prefixEmbed;
