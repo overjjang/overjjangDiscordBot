43dd38309dbcac719c3a4bda6eff5d79992de27a
overjjang99 <overjjang99@gmail.com>
2025-07-07 12:25:25 +0900

    feat: added answer generate container and emoji

--- commands/utility/askGemini.js
+++ commands/utility/askGemini.js
@@ -23,7 +23,20 @@ module.exports = {
             googleSearch: {},
         };
         const message = interaction.options.getString('질문');
-        await interaction.deferReply({});
+        await interaction.reply({
+            flags: MessageFlags.IsComponentsV2,
+            components: [
+                new ContainerBuilder()
+                    .addSeparatorComponents(new SeparatorBuilder())
+                    .addTextDisplayComponents(
+                        new TextDisplayBuilder().setContent(`${message}`)
+                    )
+                    .addSeparatorComponents(new SeparatorBuilder())
+                    .addTextDisplayComponents(
+                        new TextDisplayBuilder().setContent('<a:gemini_loading:1391617229933514792>응답을 생성하는중...')
+                    )
+            ]
+        })
         try {
             const response = await ai.models.generateContent({
                 model: 'gemini-2.5-flash',
@@ -40,6 +53,8 @@ module.exports = {
             console.log(`Gemini 답변: ${answer}`);
 
             const container = new ContainerBuilder()
+                .addTextDisplayComponents(new TextDisplayBuilder().setContent(`<:gemini_loading:1391617229933514792> ${message}`))
+                .addSeparatorComponents(new SeparatorBuilder())
                 .addTextDisplayComponents(new TextDisplayBuilder().setContent(`${answer}`));
 
             await interaction.editReply({
