e45db5c2d3ce4d68ba2df8a97fbc0dcb4a689805
overjjang99 <overjjang99@gmail.com>
2025-09-28 12:31:31 +0900

    feat: d`day function

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,28 @@
 const { Events, ActivityType, PresenceUpdateStatus} = require('discord.js');
 
+function getDDayMessage(targetDate) {
+    const now = new Date();
+    const diff = targetDate - now;
+    const days = Math.ceil(diff / (1000 * 60 * 60 * 24));
+    return `D-Day: ${days}일 남았다고`;
+}
+
 module.exports = {
     name: Events.ClientReady,
     once: true,
     execute(client) {
         console.log(`Ready! Logged in as ${client.user.tag}`);
-        client.user.setPresence({ activities: [{ name: '/도움말 | 아직 개발' }], status: PresenceUpdateStatus.DoNotDisturb });
+
+        const targetDate = new Date('2025-11-13T00:00:00+09:00');
+        let toggle = true;
+        setInterval(() => {
+            if (toggle){
+                client.user.setPresence({ activities: [{ name: '/도움말 | 아직 개발' }], status: PresenceUpdateStatus.DoNotDisturb });
+            }
+            else {
+                client.user.setPresence({ activities: [{ name: getDDayMessage(targetDate)}], status: PresenceUpdateStatus.DoNotDisturb });
+            }
+            toggle = !toggle;
+        },30000)
     },
 };
